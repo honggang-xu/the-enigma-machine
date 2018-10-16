d3de55a68d3c3e852086ce4da9060117c7837d26
honggang-xu <41971841+honggang-xu@users.noreply.github.com>
2018-10-16 13:42:53 +1300

    Add the program

---
+++ WindowsFormsAppOptionC/WindowsFormsAppOptionC/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsAppOptionC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoadRotors = new System.Windows.Forms.Button();
+            this.buttonDecodeFile = new System.Windows.Forms.Button();
+            this.buttonEncryptFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonLoadRotors
+            // 
+            this.buttonLoadRotors.Location = new System.Drawing.Point(92, 79);
+            this.buttonLoadRotors.Name = "buttonLoadRotors";
+            this.buttonLoadRotors.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadRotors.TabIndex = 0;
+            this.buttonLoadRotors.Text = "Load Rotors";
+            this.buttonLoadRotors.UseVisualStyleBackColor = true;
+            this.buttonLoadRotors.Click += new System.EventHandler(this.buttonLoadRotors_Click);
+            // 
+            // buttonDecodeFile
+            // 
+            this.buttonDecodeFile.Location = new System.Drawing.Point(92, 121);
+            this.buttonDecodeFile.Name = "buttonDecodeFile";
+            this.buttonDecodeFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonDecodeFile.TabIndex = 1;
+            this.buttonDecodeFile.Text = "Decode File";
+            this.buttonDecodeFile.UseVisualStyleBackColor = true;
+            this.buttonDecodeFile.Click += new System.EventHandler(this.buttonDecodeFile_Click);
+            // 
+            // buttonEncryptFile
+            // 
+            this.buttonEncryptFile.Location = new System.Drawing.Point(92, 162);
+            this.buttonEncryptFile.Name = "buttonEncryptFile";
+            this.buttonEncryptFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncryptFile.TabIndex = 2;
+            this.buttonEncryptFile.Text = "Encrypt File";
+            this.buttonEncryptFile.UseVisualStyleBackColor = true;
+            this.buttonEncryptFile.Click += new System.EventHandler(this.buttonEncryptFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttonEncryptFile);
+            this.Controls.Add(this.buttonDecodeFile);
+            this.Controls.Add(this.buttonLoadRotors);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoadRotors;
+        private System.Windows.Forms.Button buttonDecodeFile;
+        private System.Windows.Forms.Button buttonEncryptFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ WindowsFormsAppOptionC/WindowsFormsAppOptionC/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsAppOptionC
+{
+    public partial class Form1 : Form
+    {
+        //three arrays to store three rotors
+        string[] rotorsLargeArray;
+        string[] rotorsMiddleArray;
+        string[] rotorsSmallArray;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// load rotors
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonLoadRotors_Click(object sender, EventArgs e)
+        {
+            
+            //set filter for dialog control
+            const string FILTER = "CSV File|*.csv";
+            openFileDialog1.Filter = FILTER;
+            //declare variables
+            StreamReader inputFile;
+            string line;
+            //if user selectes file and clicks on OK button
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //open the selected file
+                inputFile = File.OpenText(openFileDialog1.FileName);
+                //while not the end of file
+                while (!inputFile.EndOfStream)
+                {
+                    //read one line from the file
+                    line = inputFile.ReadLine();
+                    //store string in a array
+                    rotorsLargeArray = line.Split(',');
+                    //read one line from the file
+                    line = inputFile.ReadLine();
+                    //store string in a array
+                    rotorsMiddleArray = line.Split(',');
+                    //read one line from the file
+                    line = inputFile.ReadLine();
+                    //store string in a array
+                    rotorsSmallArray = line.Split(',');
+                }
+                //close the file
+                inputFile.Close();
+            }
+        }
+        /// <summary>
+        /// decode the file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonDecodeFile_Click(object sender, EventArgs e)
+        {
+            //set filter for dialog control
+            const string FILTER = "Text File|*.txt";
+            openFileDialog1.Filter = FILTER;
+            saveFileDialog1.Filter = FILTER;
+            //declare variables
+            StreamReader inputFile;
+            StreamWriter outputFile;
+            string line;
+            string character;
+
+            //try catch method
+            try
+            {
+                //if user selectes file and clicks on OK button
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    //open the selected file
+                    inputFile = File.OpenText(openFileDialog1.FileName);
+                    //if user selectes file and clicks on OK button
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        //create the file
+                        outputFile = File.CreateText(saveFileDialog1.FileName);
+                        //while not the end of file
+                        while (!inputFile.EndOfStream)
+                        {
+                            //read one line from the file
+                            line = inputFile.ReadLine();
+                            //for each character
+                            for (int i = 0; i < line.Length; i++)
+                            {
+                                //store the current character into a variable
+                                character = line.Substring(i, 1);
+                                //call DecodeLetter method to decode the character
+                                string decodedCharacter = DecodeLetter(character);
+                                //shuffle the rotors
+                                ShuffleRotors();
+                                //write the decoded character to console
+                                Console.Write(decodedCharacter);
+                                //write the decode character to the new file
+                                outputFile.Write(decodedCharacter);
+                            }
+                        }
+                        Console.WriteLine("");
+                        //close the input file and output file
+                        inputFile.Close();
+                        outputFile.Close();
+                    }
+
+                }
+            }
+            //catch error
+            catch (Exception ex)
+            {
+                //show error message
+                MessageBox.Show(ex.Message);
+            }
+            
+
+        }
+        /// <summary>
+        /// decode the current letter
+        /// </summary>
+        /// <param name="character">the current letter to decode</param>
+        /// <returns></returns>
+        private string DecodeLetter(string character)
+        {
+            //declare variables
+            string letterOnMiddle = "";
+            string letterOnSmall = "";
+
+            //loop through the large rotors
+            for (int i = 0; i < rotorsLargeArray.Length; i++)
+            {
+                //if the character is found on large rotor
+                if (rotorsLargeArray[i] == character)
+                {
+                    //store the character at the same position on the middle rotor
+                    letterOnMiddle = rotorsMiddleArray[i];
+                }
+            }
+            //loop through the large rotors
+            for (int count = 0; count < rotorsLargeArray.Length; count++)
+            {
+                //if the character on the middle rotor is found on large rotor
+                if (rotorsLargeArray[count] == letterOnMiddle)
+                {
+                    //store the character at the same position on the small rotor
+                    letterOnSmall = rotorsSmallArray[count];
+                }
+            }
+            //return character on the small rotor
+            return letterOnSmall;
+        }
+        /// <summary>
+        /// shuffle the rotors
+        /// </summary>
+        private void ShuffleRotors()
+        {
+            //if the space gets to the end of the middle rotor
+            if (rotorsMiddleArray[rotorsMiddleArray.Length - 1] == " ")
+            {
+                //store the current last character in large rotor into a variable
+                string lastLargeCharacter = rotorsLargeArray[rotorsLargeArray.Length - 1];
+                //for each character to shuffle starting from the end
+                for (int i = rotorsLargeArray.Length - 1; i > 0; i--)
+                {
+                    //current character equals the character before it
+                    rotorsLargeArray[i] = rotorsLargeArray[i - 1];
+                }
+                //shift the stored last character to the first character in the array
+                rotorsLargeArray[0] = lastLargeCharacter;
+            }
+
+            //if the space gets to the end of the small rotor
+            if (rotorsSmallArray[rotorsSmallArray.Length - 1] == " ")
+            {
+                //store the current last character in middel rotor into a variable
+                string lastMiddleCharacter = rotorsMiddleArray[rotorsMiddleArray.Length - 1];
+                //for each character to shuffle starting from the end
+                for (int i = rotorsMiddleArray.Length - 1; i > 0; i--)
+                {
+                    //current character equals the character before it
+                    rotorsMiddleArray[i] = rotorsMiddleArray[i - 1];
+                }
+                //shift the stored last character to the first character in the array
+                rotorsMiddleArray[0] = lastMiddleCharacter;
+            }
+
+            //store the current last character into a variable
+            string lastSmallCharacter = rotorsSmallArray[rotorsSmallArray.Length - 1];
+            //for each character to shuffle starting from the end
+            for (int i = rotorsSmallArray.Length - 1; i > 0; i--)
+            {
+                ////current character equals the character before it
+                rotorsSmallArray[i] = rotorsSmallArray[i - 1];
+            }
+            //shift the stored last character to the first character in the array
+            rotorsSmallArray[0] = lastSmallCharacter;
+        }
+        /// <summary>
+        /// encrypt the current character
+        /// </summary>
+        /// <param name="character">the current character to encrypt</param>
+        /// <returns></returns>
+        private string EncryptLetter(string character)
+        {
+            //declare variables
+            string LetterOnLarge = "";
+            string encryptedLetter = "";
+            //loop through the small rotors
+            for (int i = 0; i < rotorsSmallArray.Length; i++)
+            {
+                //if the character is found on small rotor
+                if (rotorsSmallArray[i] == character)
+                {
+                    //store the character at the same position on the large rotor
+                    LetterOnLarge = rotorsLargeArray[i];
+                }
+            }
+            //loop through the middle rotors
+            for (int count = 0; count < rotorsMiddleArray.Length; count++)
+            {
+                //if the character is found on middle rotor
+                if (rotorsMiddleArray[count] == LetterOnLarge)
+                {
+                    ////store the character at the same position on the large rotor
+                    encryptedLetter = rotorsLargeArray[count];
+                }
+            }
+            //return the character on the large rotor
+            return encryptedLetter;
+        }
+        /// <summary>
+        /// encrypt the file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonEncryptFile_Click(object sender, EventArgs e)
+        {
+            //set filter for dialog control
+            const string FILTER = "Text File|*.txt";
+            openFileDialog1.Filter = FILTER;
+            saveFileDialog1.Filter = FILTER;
+            //declare variables
+            StreamReader inputFile;
+            StreamWriter outputFile;
+            string line;
+            string character;
+
+            //try catch method
+            try
+            {
+                //if user selectes file and clicks on OK button
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    //open the selected file
+                    inputFile = File.OpenText(openFileDialog1.FileName);
+                    //if user selectes file and clicks on OK button
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        //create the file
+                        outputFile = File.CreateText(saveFileDialog1.FileName);
+                        //while not the end of the file
+                        while (!inputFile.EndOfStream)
+                        {
+                            ////read one line from the file
+                            line = inputFile.ReadLine();
+                            //for each character
+                            for (int i = 0; i < line.Length; i++)
+                            {
+                                //store the current character into a variable
+                                character = line.Substring(i, 1);
+                                //call EncryptLetter method to encrypt the character
+                                string encryptedCharacter = EncryptLetter(character);
+                                //write the encrypted character to console
+                                Console.Write(encryptedCharacter);
+                                //write the encrypted character to file
+                                outputFile.Write(encryptedCharacter);
+                                //shuffle rotors
+                                ShuffleRotors();
+                            }
+                        }
+                        Console.WriteLine("");
+                        //close the input file and output file
+                        inputFile.Close();
+                        outputFile.Close();
+                    }
+
+                }
+            }
+            //catch error
+            catch (Exception ex)
+            {
+                //show error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
